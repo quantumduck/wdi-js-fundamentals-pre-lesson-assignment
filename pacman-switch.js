eeb1009d1831a087543b8320b2421afa80d68da1
quantumduck <quantum.duck@hotmail.com>
2017-04-26 10:00:01 -0400

    swithing

---
+++ pacman-switch.js
@@ -0,0 +1,21 @@
+var ghost = 'Sllinky';
+var colour;
+
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Pinky':
+    colour = 'Pink';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+  default :
+    colour = 'Unknown';
+}
+
+console.log(ghost + ' is the colour ' + colour);
